5391dfe5ec863606e2529ad9511ebe5c8d0bdfc8
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2024-07-08 22:04:38 +0800

    Add GitHub Actions workflow and deployment script

--- MT_BE/bin/bash.sh
+++ MT_BE/bin/bash.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+echo "Fetching DATABASE_URL from Heroku..."
 DATABASE_URL=$(heroku config:get DATABASE_URL -a aiman-db)
+echo "DATABASE_URL fetched: $DATABASE_URL"
 
+echo "Exporting DATABASE_URL as environment variable..."
 export DATABASE_URL
 
-node ./MT_BE/bin/www
+echo "Starting application..."
+node ./MT_BE/bin/www &
+APP_PID=$!
+
+
+while kill -0 $APP_PID >/dev/null 2>&1; do
+  echo "Application is running..."
+  sleep 60
+done
+
+echo "Application stopped running."
