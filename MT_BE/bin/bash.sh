418e4328ece92a8af2c128fe82a5ff9acb467365
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2024-07-08 21:38:24 +0800

    Add GitHub Actions workflow and deployment script

--- MT_BE/bin/bash.sh
+++ MT_BE/bin/bash.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Fetch the latest DATABASE_URL
 DATABASE_URL=$(heroku config:get DATABASE_URL -a aiman-db)
 
-# Export it as an environment variable (useful for local development)
 export DATABASE_URL
 
-# Restart your application to pick up the new credentials (if necessary)
-node ./bin/www
+node ./MT_BE/bin/www
