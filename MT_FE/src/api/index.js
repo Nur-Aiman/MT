7e8b6cd729fa8744114575905301da656a4c00ef
Nur-Aiman <64392281+Nur-Aiman@users.noreply.github.com>
2025-03-05 21:43:22 +0800

    update BE url from render to heroku

--- MT_FE/src/api/index.js
+++ MT_FE/src/api/index.js
@@ -1,4 +1,6 @@
-export const HOST = 'https://murajaah-tracker-api.onrender.com'
+export const HOST = 'https://murajaah-tracker-api-4ded06185bd3.herokuapp.com'
 
 // local : http://localhost:8080
-// production : https://murajaah-tracker-api.onrender.com
+// production : 
+// render --> https://murajaah-tracker-api.onrender.com
+// heroku --> https://murajaah-tracker-api-4ded06185bd3.herokuapp.com
