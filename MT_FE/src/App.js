5cba9bea444295f9d0d3f21d2fe8040f3e1049c3
Nur-Aiman <nur_aiman71099@yahoo.com>
2026-01-22 08:40:23 +0800

    change router library

--- MT_FE/src/App.js
+++ MT_FE/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import SurahList from './pages/SurahList'
 import Tadabbur from './pages/Tadabbur' 
 import Login from './pages/Login'
